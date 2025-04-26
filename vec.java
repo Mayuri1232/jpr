18733b025342868fb1bafa8e0472ca5572dc2593
Yusuf Dohadwala <84133819+Yusuf24-tech@users.noreply.github.com>
2025-04-26 18:04:51 +0530

    Update vec.java

--- vec.java
+++ vec.java
@@ -4,33 +4,22 @@ class Vec
 public static void main(String args[])
 {
 Vector v=new Vector();
-v.add(10);
-v.add(20);
-v.add(30);
-v.add(40.546);
-v.add(12.34);
-v.add(1000);
-v.addElement("Vector End");
-System.out.println("Original Vectore Elements:");
-for(int i=0;i<v.size();i++)
-{
-System.out.print(" " +v.elementAt(i) +" ");
-}
-System.out.println("Capacity : "+v.capacity());
-System.out.println("Contians 20? "+v.contains(20));
-System.out.println("Element at 5th location : "+v.elementAt(5));
-System.out.println("Element at 2nd index using get() :"+v.get(2));
-System.out.println("First Element : "+v.firstElement());
-System.out.println("Last element  : "+v.lastElement());
-System.out.println("Index of 12.24 : "+v.indexOf(12.24));
-System.out.println("Hash Code : "+v.hashCode());
-System.out.println("Last index of 20 : "+v.lastIndexOf(20));
-System.out.println(v.remove(4));
-v.removeElementAt(3);
-System.out.println(v.removeAll(v));
-System.out.println("Size afert Element revoing :"+v.size());
-System.out.println("Is empty Vector:"+v.isEmpty());
-System.out.println(v);
+Integer i1=new Integer(10);
+Float f1=new Float(13.4);
+  String str=new String("Hello");
+  v.addElement(i1);
+  v.addElement(f1);
+  v.addElement(s1);
+  v.removeElementAt(1);
+  Double d1= new Double(14.5);
+  v.insertElementAt(d1,1);
+  System.out.println(v);
+  v.removeElement(i1);
+  System.out.println(v.elementAt(2));
+  System.out.println(v.size());
+  System.out.println(v.removeAllElement());
+
+  
 }
 }
 
